0115f35b919223a4a54e67646c0515b856129250
VelmaAkoch <velmaakochi1@gmail.com>
2022-05-24 16:17:29 +0300

    add files added

--- app/src/main/java/com/velmamukanga/myrestaurants/MainActivity.java
+++ app/src/main/java/com/velmamukanga/myrestaurants/MainActivity.java
@@ -3,8 +3,10 @@ package com.velmamukanga.myrestaurants;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
